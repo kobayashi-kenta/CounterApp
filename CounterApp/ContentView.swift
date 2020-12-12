1a665084bec153c3057a6cf65ca4e71b2079ad2e
kobayashi-kenta <12321klj@gmail.com>
2020-12-12 16:41:13 +0900

    finish

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -11,9 +11,22 @@ struct ContentView: View {
     @State var number = 0
     var body: some View {
         VStack {
+            ZStack{
+            Image("counter")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
             Text("\(number)")
+                .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+            }
             Button(action: {self.number += 1}) {
                 Text("カウント")
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.white)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .background(Color.blue)
+                    .cornerRadius(10.0)
+
             }
         }
     }
