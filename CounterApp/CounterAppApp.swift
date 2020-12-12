c24a166a1186eeacd3a7ff9017e32dfa9c550e1b
kobayashi-kenta <12321klj@gmail.com>
2020-12-12 10:01:53 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Kenta on 2020/12/12.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
